4838b3be7f211124487987a92939fe23f42f56ad
vikas <vikas4goyal@gmail.com>
2021-07-22 21:39:00 +0530

    Initial Commit

---
+++ Registration/RegistrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationApp.swift
+//  Registration
+//
+//  Created by vikas on 22/07/21.
+//
+
+import SwiftUI
+
+@main
+struct RegistrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
